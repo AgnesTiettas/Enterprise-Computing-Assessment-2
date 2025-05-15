8ee9f5944379c007f984a6634e751c4ab0f87b4e
AgnesTiettas <agnes.tiettas1@education.nsw.gov.au>
2025-05-15 23:56:21 +1000

    V5-15/5/25
    I completed creating a functional side toggle bar with HTML, CSS and JavaScript. Although navigation bar was supposed to be on the side and when the screen size changes it moved to the top it was changed as it was not an essential feature. There were some challenges encountered which were that sometimes the naming across the different files wouldn't be correct and the navigation bar wouldn't work so I would need to go over them again to identify and inconsistencies with the naming. There was a feature added so that when the navigation bar is collapsed the arrow flips sides to represent expanding the navigation bar, although it is a very simple feature it can help improve the user interface as it makes the design more logical.

---
+++ Home.js
@@ -0,0 +1,25 @@
+
+//Initalise values 
+const sidebar =document.querySelector(".sidebar");
+const sidebarToggler = document.querySelector(".sidebar-toggler");
+const menuToggler = document.querySelector(".menu-toggler");
+
+//Makes sure that the height values of the sidebar match 
+
+//Inital Height
+let NormalSidebarHeight= "calc(100vh - 32px)";
+
+//Height when collapsed
+let collapsedSidebarHeight ="56px";
+
+//Make the sidebar collapse 
+sidebarToggler.addEventListener("click", () => {
+    sidebar.classList.toggle("collapsed");
+});
+
+//Update the height of the sidebar and menu toggle the text
+const toggleMenu = (isMenuActive) => {
+  sidebar.style.height = isMenuActive ? `${sidebar.scrollHeight}px` : collapsedSidebarHeight;
+  menuToggler.querySelector("span").innerText = isMenuActive ? "close" : "menu";
+}
+
